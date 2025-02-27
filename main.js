04f8712eeafe10f60933a7047cc5846090283b96
Anton <52106716+Xebit8@users.noreply.github.com>
2025-02-27 17:42:10 +0300

    modify dsv conversion algorithm (flatten the structure)

--- main.js
+++ main.js
@@ -5,74 +5,74 @@ import { transliterate } from "transliteration";
 import { promises as fs } from "fs";
 
 
-const data = [];
-(function generateData() {
-    for (let i = 0; i < 1200; i++)
-    {
-        let fullName = fakerRU.person.fullName();
-        let names = fullName.split(' ');
-        let firstName = transliterate(names[0].toLowerCase());
-        let lastName = transliterate(names[1].toLowerCase());
+// const data = [];
+// (function generateData() {
+//     for (let i = 0; i < 1200; i++)
+//     {
+//         let fullName = fakerRU.person.fullName();
+//         let names = fullName.split(' ');
+//         let firstName = transliterate(names[0].toLowerCase());
+//         let lastName = transliterate(names[1].toLowerCase());
 
-        let emails = [fakerRU.internet.email({firstName: firstName, lastName: lastName, provider: "mail.ru"})];
-        let number_of_emails = fakerRU.number.int({min: 0, max: 2});
-        for (let i = 0; i < number_of_emails; i++)
-        {
-            emails.push(fakerRU.internet.email());
-        }
+//         let emails = [fakerRU.internet.email({firstName: firstName, lastName: lastName, provider: "mail.ru"})];
+//         let number_of_emails = fakerRU.number.int({min: 0, max: 2});
+//         for (let i = 0; i < number_of_emails; i++)
+//         {
+//             emails.push(fakerRU.internet.email());
+//         }
 
-        let days_past_registration = fakerRU.number.int({min: 100, max: 2500});
-        let days_past_last_auth = fakerRU.number.int({min: 1, max: 90});
-        let days_past_birth = fakerRU.number.int({min: 5000, max: 20000});
+//         let days_past_registration = fakerRU.number.int({min: 100, max: 2500});
+//         let days_past_last_auth = fakerRU.number.int({min: 1, max: 90});
+//         let days_past_birth = fakerRU.number.int({min: 5000, max: 20000});
 
-        let publications = [];
-        let number_of_pubs = fakerRU.number.int({min: 1, max: 3});
-        for (let i = 0; i < number_of_pubs; i++) {
-            let days_past_publication = fakerRU.number.int({min: 1, max: 100});
+//         let publications = [];
+//         let number_of_pubs = fakerRU.number.int({min: 1, max: 3});
+//         for (let i = 0; i < number_of_pubs; i++) {
+//             let days_past_publication = fakerRU.number.int({min: 1, max: 100});
 
-            let comments = [];
-            let number_of_comments = fakerRU.number.int({min: 0, max: 3});
-            for (let i = 0; i < number_of_comments; i++) {
-                comments.push({
-                    user_id: fakerRU.number.int({min: 1, max: 1200}),
-                    comment_text: fakerRU.lorem.paragraph()
-                })
-            }
+//             let comments = [];
+//             let number_of_comments = fakerRU.number.int({min: 0, max: 3});
+//             for (let i = 0; i < number_of_comments; i++) {
+//                 comments.push({
+//                     user_id: fakerRU.number.int({min: 1, max: 1200}),
+//                     comment_text: fakerRU.lorem.paragraph()
+//                 })
+//             }
 
-            publications.push({
-                title: fakerRU.lorem.sentence(),
-                description: fakerRU.lorem.paragraph(),
-                pages: fakerRU.number.int({min: 1, max: 20}),
-                category: fakerRU.lorem.word(),
-                publication_data: fakerRU.date.recent({days: days_past_publication}).toLocaleDateString(),
-                comments: comments
-            })
-        }
+//             publications.push({
+//                 title: fakerRU.lorem.sentence(),
+//                 description: fakerRU.lorem.paragraph(),
+//                 pages: fakerRU.number.int({min: 1, max: 20}),
+//                 category: fakerRU.lorem.word(),
+//                 publication_data: fakerRU.date.recent({days: days_past_publication}).toLocaleDateString(),
+//                 comments: comments
+//             })
+//         }
 
-        data.push({
-            user_id: i+1,
-            full_name: fullName,
-            email: emails,
-            registration_date: fakerRU.date.recent({days: days_past_registration}).toLocaleDateString(),
-            last_authorization_date: fakerRU.date.recent({days: days_past_last_auth}).toLocaleDateString(),
-            status: fakerRU.helpers.arrayElement(["Подтвержден", "Не подтвержден"]),
-            publications: publications,
-            birth_date: fakerRU.date.recent({days: days_past_birth}).toLocaleDateString(),
-            gender: fakerRU.helpers.arrayElement(["Мужской", "Женский"]),
-        });
-    }
-})();
+//         data.push({
+//             user_id: i+1,
+//             full_name: fullName,
+//             email: emails,
+//             registration_date: fakerRU.date.recent({days: days_past_registration}).toLocaleDateString(),
+//             last_authorization_date: fakerRU.date.recent({days: days_past_last_auth}).toLocaleDateString(),
+//             status: fakerRU.helpers.arrayElement(["Подтвержден", "Не подтвержден"]),
+//             publications: publications,
+//             birth_date: fakerRU.date.recent({days: days_past_birth}).toLocaleDateString(),
+//             gender: fakerRU.helpers.arrayElement(["Мужской", "Женский"]),
+//         });
+//     }
+// })();
 
-(async function exportJSON()
-{
-    try {
-        await fs.writeFile("dummy_data.json", JSON.stringify(data), null, 4);
-        console.log("JSON File successfully created!");
-    }
-    catch (error) {
-        console.error(error);
-    }
-}());
+// (async function exportJSON()
+// {
+//     try {
+//         await fs.writeFile("dummy_data.json", JSON.stringify(data), null, 4);
+//         console.log("JSON File successfully created!");
+//     }
+//     catch (error) {
+//         console.error(error);
+//     }
+// }());
 
 let imported_data = {};
 async function importJSON()
@@ -101,26 +101,46 @@ imported_data = await importJSON();
     }
 }());
 
-async function convertToDSV(imported_data, delimiter = '\t')
-{
+async function convertToDSV(imported_data, delimiter = '\t') {
     try {
-        let headers = Object.keys(imported_data[0]);
-        // Go through each row
-        let rows = imported_data.map(row => 
-            headers.map(field => {
-            let value = row[field];
-            if (field === "publications") {
-                // Put nested object into 1 value
-                value = JSON.stringify(row["publications"]);
+        let headers = [
+            "user_id", "full_name", "email", "registration_date", "last_authorization_date", "status", "birth_date", "gender",
+            "publication_title", "publication_description", "publication_pages", "publication_category", "publication_date",
+            "comment_user_id", "comment_text"
+        ];
+
+        let rows = [];
+
+        // Go through each user
+        for (let user of imported_data) {
+            // Go through each publication of the user
+            for (let publication of user.publications) {
+                // If there are no comments, add a row with empty comment fields
+                if (publication.comments.length === 0) {
+                    let row = [
+                        user.user_id, user.full_name, user.email.join(", "), user.registration_date, user.last_authorization_date, user.status, user.birth_date, user.gender,
+                        publication.title, publication.description, publication.pages, publication.category, publication.publication_data,
+                        "", ""
+                    ];
+                    rows.push(row.join(delimiter));
+                } else {
+                    // Go through each comment of the publication
+                    for (let comment of publication.comments) {
+                        let row = [
+                            user.user_id, user.full_name, user.email.join(", "), user.registration_date, user.last_authorization_date, user.status, user.birth_date, user.gender,
+                            publication.title, publication.description, publication.pages, publication.category, publication.publication_data,
+                            comment.user_id, comment.comment_text
+                        ];
+                        rows.push(row.join(delimiter));
+                    }
+                }
             }
-            return value;
-            }).join(delimiter)
-        );
+        }
+
         // Inserting headers to the top
         rows.unshift(headers.join(delimiter));
         return rows.join('\n');
-    }
-    catch (error) {
+    } catch (error) {
         console.error(error);
     }
 };
